0a27086d817023da7424ab1ccfb0450308a262b5
bydrim <22540269+bydrim@users.noreply.github.com>
2026-02-04 17:42:23 +0000

    added tailwind and daisyui, added custom dark and light theme, added theme selector

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
-    <h1>hello index page</h1>
+    <h1 class="text-3xl font-bold underline">hello index page</h1>
+    <button class="btn btn-primary">Button</button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     devServer: {
         port: 3030
-    }
+    },
+    css: ["./app/assets/css/main.css"],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
 })
