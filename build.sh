9e6811fe5ffface68db33b81270371c171b5d545
bydrim <22540269+bydrim@users.noreply.github.com>
2026-02-07 00:46:21 +0000

    changed woodpecker build image (to node). in build.sh fixed error handling, added info prints, adapted to pnpm and nuxt

--- build.sh
+++ build.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
 # in case of error, print error line and exit.
-hande_error() {
-    echo "Error on line $1. Build has probably failed."
+handle_error() {
+    echo "Error on line $1. Aborting the building process."
     exit 1
 }
 
 trap 'handle_error $LINENO' ERR
 
-# create outputs folder, copy src and rename with timestamp
+echo "## Project building process is starting."
+
+echo "## Installing pnpm 10.28.1 .."
+npm install -g pnpm@10.28.1
+
+echo "## Installing project dependencies with pnpm .."
+pnpm i --frozen-lockfile
+
+echo "## Generating static files .."
+pnpm generate
+
+# create outputs folder, copy generated project and rename with timestamp
+echo "## Copying generated files .."
 if [[ ! -d ./outputs ]]; then
     mkdir ./outputs
 fi
-cp -r ./src ./outputs
+cp -r ./.outputs/public ./outputs
 
+# year month day hour minute second
 timestamp=$(date +%Y%m%d%H%M%S)
-mv ./outputs/src "./outputs/$timestamp"
+mv ./outputs/public "./outputs/$timestamp"
+echo "## Static content copied to './outputs/$timestamp'."
 
 # create relative symbolic link for the latest build so server can always target
 # the latest without config change
@@ -23,3 +37,6 @@ if [[ -h ./outputs/latest ]]; then
     rm -f ./outputs/latest
 fi
 ln -sr "./outputs/$timestamp" ./outputs/latest
+echo "## Symbolic link created."
+
+echo "\n## Project building completed."
