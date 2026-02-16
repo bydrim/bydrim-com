106d7719b0fa76fb2253a891e3ac1d79853ac9b9
bydrim <22540269+bydrim@users.noreply.github.com>
2026-02-16 12:41:58 +0000

    'projects' page placeholder

---
+++ app/pages/projects.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-full flex flex-col justify-start items-center-safe lg:justify-center-safe lg:items-start">
+        <p class="text-4xl font-bold">Projects</p>
+        <!--
+        <p v-for="_ in new Array(100)" class="text-2xl text-center">this is project</p>
+        -->
+        <p class="text-2xl">Coming ..</p>
+    </div>
+</template>
